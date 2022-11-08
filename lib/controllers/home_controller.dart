4e90868f05e914e3bece19a2f99f9e2e472fef7d
Daniel <marillancadaniel@gmail.com>
2022-11-08 00:00:14 -0300

    archivos base para los controladores

---
+++ lib/controllers/home_controller.dart
@@ -0,0 +1,42 @@
+import 'package:smart_routine/models/task.dart';
+//import 'package:smart_routine/providers/database_provider.dart';
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  final Rx<DateTime> _selectedDate = DateTime.now().obs;
+
+  DateTime get selectedDate => _selectedDate.value;
+
+  final Rx<List<Task>> _myTasks = Rx<List<Task>>([]);
+
+  List<Task> get myTasks => _myTasks.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+    getTasks();
+  }
+
+  getTasks() async {
+    final List<Task> tasksFromDB = [];
+    // List<Map<String, dynamic>> tasks = await DatabaseProvider.queryTasks();
+    // tasksFromDB.assignAll(
+    //     tasks.reversed.map((data) => Task().fromJson(data)).toList());
+    // _myTasks.value = tasksFromDB;
+  }
+
+  Future<int> deleteTask(String id) async {
+    return 0;
+    //return await DatabaseProvider.deleteTask(id);
+  }
+
+  Future<int> upDateTask(String id) async {
+    return 0;
+    //return await DatabaseProvider.updateTask(id);
+  }
+
+  updateSelectedDate(DateTime date) {
+    _selectedDate.value = date;
+    getTasks();
+  }
+}
